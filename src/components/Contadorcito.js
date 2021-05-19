6b4b0f05f4b612715144c8cc6ddd84417be44e61
Daniela <danillano97@gmail.com>
2021-05-19 01:18:23 -0300

    con el reducerrr

--- src/components/Contadorcito.js
+++ src/components/Contadorcito.js
@@ -1,15 +1,18 @@
 import { useContext } from 'react';
 import {PalContador} from '../context/PalContador';
+import './Contadorcito.css'
 
 function Contadorcito() {
     const valor = useContext(PalContador)
 
     return (
-        <div>
-            <button onClick={valor.sumarNumerito}>Sumar</button>
-            <div>{valor.contador}</div>
-            <button onClick={valor.restarNumerito}>Restar</button>
-            <p>{valor.mensaje}</p>
+        <div className="container">
+            <div className="containerChikito">
+                <button onClick={valor.sumarNumerito}>Sumar</button>
+                <h1 className="numerito">{valor.contador.contadorcito}</h1>
+                <button onClick={valor.restarNumerito}>Restar</button>
+            </div>
+            <p>{valor.contador.error}</p>
         </div>
     )
 }
--- src/context/PalContadorProvider.js
+++ src/context/PalContadorProvider.js
@@ -1,27 +1,63 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { PalContador } from './PalContador';
 
 function Dealer({children}) {
 
-    const [contador, setContador] = useState(0);
-    const [mensaje, setMensaje] = useState("");
+    // const [contador, setContador] = useState(0);
+    // const [mensaje, setMensaje] = useState("");
+
+    // const sumarNumerito = () => {
+    //         setContador(contador + 1)
+    //         setMensaje("")
+    //     }
+
+    // const restarNumerito = () => {
+    //     if (contador === 0) {
+    //         setMensaje("El número no puede ser menor a 0")
+    //     return;
+    //     }
+    //     setContador(contador - 1)
+    // }
+
+    const reducer = (estado, action) => {
+        switch (action) {
+            case "SUMAR":
+                return {
+                    ...estado,
+                    error: null,
+                    contadorcito: estado.contadorcito + 1
+                };
+            case "RESTAR":
+                return {
+                    ...estado,
+                    error: estado.contadorcito === 0 ? "No se puede mas, rey" : null,
+                    contadorcito: estado.contadorcito < 1 ? 0 : estado.contadorcito - 1
+                };
+        
+            default:
+                break;
+        }
+    }
+
+    const estadoInicial = {
+        contadorcito: 0,
+        error: null
+    }
+
+    const [contador, dispatch] = useReducer(reducer, estadoInicial);
 
     const sumarNumerito = () => {
-            setContador(contador + 1)
-            setMensaje("")
-        }
+        dispatch("SUMAR")
+    }
 
     const restarNumerito = () => {
-        if (contador === 0) {
-            setMensaje("El número no puede ser menor a 0")
-        return;
-        }
-        setContador(contador - 1)
+        dispatch("RESTAR")
     }
+
+
 
     const value = {
         contador,
-        mensaje,
         sumarNumerito,
         restarNumerito,
     }
