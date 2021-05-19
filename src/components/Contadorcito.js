05222b599c22243d389789f34e4ad5b44f8d4166
Daniela <danillano97@gmail.com>
2021-05-19 20:15:59 -0300

    context, reducer y styled-components

--- src/components/Contadorcito.js
+++ src/components/Contadorcito.js
@@ -1,19 +1,51 @@
 import { useContext } from 'react';
 import {PalContador} from '../context/PalContador';
-import './Contadorcito.css'
+import styled from 'styled-components'
 
 function Contadorcito() {
     const valor = useContext(PalContador)
 
+    const StyledBtn = styled.button `
+        padding: 10px;
+        border-radius: 15px;
+        cursor: pointer;
+        outline: none;
+        font-size: 15px;
+        font-weight: bold;
+        background: white;
+        border: 2px solid ${props => props.brd === "palevioletred" ? "palevioletred" : "grey"};
+        color: ${props => props.letra === "grey" ? "grey" : "palevioletred"};
+    `
+
+    const Container = styled.div `
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    color: #FFF;
+    min-height: 100vh;
+    `
+
+    const Numerito = styled.h1 `
+    margin: 40%;
+    font-size: 5rem;
+    `
+
+    const MiniContainer = styled.div `
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    `
+
     return (
-        <div className="container">
-            <div className="containerChikito">
-                <button onClick={valor.sumarNumerito}>Sumar</button>
-                <h1 className="numerito">{valor.contador.contadorcito}</h1>
-                <button onClick={valor.restarNumerito}>Restar</button>
-            </div>
-            <p>{valor.contador.error}</p>
-        </div>
+        <Container>
+            <MiniContainer>
+                <StyledBtn brd="palevioletred" letra="grey" onClick={valor.sumarNumerito}>Sumar</StyledBtn>
+                <Numerito>{valor.contador.contadorcito}</Numerito>
+                <StyledBtn brd="grey" letra="palevioletred" onClick={valor.restarNumerito}>Restar</StyledBtn>
+            </MiniContainer>
+            {valor.contador.error && <p>{valor.contador.error}</p>}
+        </Container>
     )
 }
 
